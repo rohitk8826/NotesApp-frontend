ba2b386e880625e6a76eae4adae94d119d666ef3
rohitk8826 <rohit8826k@gmail.com>
2025-08-29 13:16:28 +0530

    update Login.jsx api base url

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -23,7 +23,10 @@ export default function Login() {
     setLoading(true);
 
     try {
-      const { data } = await api.post("/auth/login", { email, password });
+      const { data } = await api.post(
+        `${import.meta.env.VITE_API_URL}/auth/login`,
+        { email, password }
+      );
       login(data); // ✅ AuthContext now handles token & localStorage
       navigate("/notes");
     } catch (err) {

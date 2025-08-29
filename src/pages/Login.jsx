9034567617eaa0c2e7d7e43f60f2173f9a0aa55d
rohitk8826 <rohit8826k@gmail.com>
2025-08-29 13:37:41 +0530

    update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -23,7 +23,7 @@ export default function Login() {
     setLoading(true);
 
     try {
-      const { data } = await api.post("/auth/login", { email, password });
+      const { data } = await api.post("api/auth/login", { email, password });
       login(data); // ✅ AuthContext now handles token & localStorage
       navigate("/notes");
     } catch (err) {
